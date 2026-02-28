a348c3d319006752cd83c24ac4f5c1232569d11f
bharathi9989 <er.velubharathi.s@gmail.com>
2026-02-28 18:19:28 +0530

    feat : added a Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Hero />
+    </>
+  );
 }
 
-export default App
+export default App;
