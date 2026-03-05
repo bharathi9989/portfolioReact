acce20ea2bb25a80c502c665b7a78e1bd287b54c
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-05 18:40:32 +0530

    feat : added a naviagation styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,18 @@ function Home() {
   return (
     <>
       <HeroScoreboard />
+
       <PitchNavigation />
-      <section className="py-40 text-center">
-        <h2 className="text-3xl">Projects Section Coming Soon</h2>
+
+      <section id="projects" className="py-40 text-center">
+        <h2 className="text-3xl">Projects Section</h2>
       </section>
 
-      <section className="py-40 text-center">
+      <section id="skills" className="py-40 text-center">
         <h2 className="text-3xl">Skills Section</h2>
       </section>
 
-      <section className="py-40 text-center">
+      <section id="contact" className="py-40 text-center">
         <h2 className="text-3xl">Contact Section</h2>
       </section>
     </>
