44c0d3e9e6616d797d75dd0f7657a7ea9d7bccdc
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-07 06:56:15 +0530

    fix : solving case sensitive problems

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-import github from "../assets/GitHub.png";
+import github from "../assets/github.png";
 import linkedin from "../assets/linkedin.png";
 import mail from "../assets/Mail.png";
 
